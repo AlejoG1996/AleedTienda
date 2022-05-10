7b7a7f27e47fecb184a66796cff042144a2b5d34
AlejoG1996 <89146947+AlejoG1996@users.noreply.github.com>
2022-05-10 12:48:59 -0500

    Combos

---
+++ AleedShooping/AleedShooping/Helpers/ICombosHelper.cs
@@ -0,0 +1,18 @@
+﻿using AleedShooping.Data.Entities;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace AleedShooping.Helpers
+{
+    public interface ICombosHelper
+    {
+        Task<IEnumerable<SelectListItem>> GetComboCategoriesAsync();
+
+        Task<IEnumerable<SelectListItem>> GetComboCategoriesAsync(IEnumerable<Category> filter);
+
+        Task<IEnumerable<SelectListItem>> GetComboCountriesAsync();
+
+        Task<IEnumerable<SelectListItem>> GetComboStatesAsync(int countryId);
+
+        Task<IEnumerable<SelectListItem>> GetComboCitiesAsync(int stateId);
+    }
+}
